a37333651ce7124d9477231f2267976ddb8e813c
Andreas Pfohl <mail@apfohl.com>
2024-12-23 20:53:49 +0100

    Added day 8 part two

--- src/day8.rs
+++ src/day8.rs
@@ -25,7 +25,38 @@ fn add_or_insert(
         .push((x, y))
 }
 
-fn calculate_antinodes(
+fn get_antennas(puzzle: &mut Vec<Vec<char>>) -> HashMap<char, Vec<(usize, usize)>> {
+    let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+
+    for y in 0..puzzle.len() {
+        for x in 0..puzzle[0].len() {
+            match puzzle[y][x] {
+                '.' => continue,
+                character => add_or_insert(&mut antennas, character, x, y),
+            }
+        }
+    }
+
+    antennas
+}
+
+fn load_antennas() -> Result<
+    (
+        Vec<Vec<char>>,
+        (usize, usize),
+        HashMap<char, Vec<(usize, usize)>>,
+    ),
+    Error,
+> {
+    let mut puzzle = load_puzzle()?;
+    let dimensions = (puzzle[0].len(), puzzle.len());
+
+    let antennas = get_antennas(&mut puzzle);
+
+    Ok((puzzle, dimensions, antennas))
+}
+
+fn calculate_antinodes_one(
     lhs: (usize, usize),
     rhs: (usize, usize),
     dimensions: (usize, usize),
@@ -57,15 +88,19 @@ fn test_calculate_antinodes() {
     ];
 
     cases.iter().for_each(|(lhs, rhs, expected)| {
-        let result = calculate_antinodes(*lhs, *rhs, (12, 12));
+        let result = calculate_antinodes_one(*lhs, *rhs, (12, 12));
         assert_eq!(result, *expected);
     })
 }
 
-fn calculate_antinodes_for_antennas(
+fn calculate_antinodes_for_antennas<F>(
     points: &Vec<(usize, usize)>,
     dimensions: (usize, usize),
-) -> Vec<(usize, usize)> {
+    func: &F,
+) -> Vec<(usize, usize)>
+where
+    F: Fn((usize, usize), (usize, usize), (usize, usize)) -> Vec<(usize, usize)>,
+{
     if points.is_empty() {
         return vec![];
     }
@@ -74,8 +109,8 @@ fn calculate_antinodes_for_antennas(
     let tail = &points[1..].to_vec();
 
     tail.into_iter()
-        .flat_map(|rhs| calculate_antinodes(lhs, *rhs, dimensions))
-        .chain(calculate_antinodes_for_antennas(tail, dimensions).into_iter())
+        .flat_map(|rhs| func(lhs, *rhs, dimensions))
+        .chain(calculate_antinodes_for_antennas(tail, dimensions, func).into_iter())
         .collect()
 }
 
@@ -83,7 +118,7 @@ fn calculate_antinodes_for_antennas(
 fn test_calculate_antinodes_for_antennas() {
     let antennas = vec![(8, 1), (5, 2), (7, 3), (4, 4)];
 
-    let antinodes = calculate_antinodes_for_antennas(&antennas, (12, 12));
+    let antinodes = calculate_antinodes_for_antennas(&antennas, (12, 12), &calculate_antinodes_one);
     let expected = vec![
         (11, 0),
         (2, 3),
@@ -102,40 +137,63 @@ fn test_calculate_antinodes_for_antennas() {
 
 #[test]
 fn part_one() -> Result<(), Error> {
-    let mut puzzle = load_puzzle()?;
-    let dimensions = (puzzle[0].len(), puzzle.len());
+    let (mut puzzle, dimensions, antennas) = load_antennas()?;
 
-    // println!("{:?}", dimensions);
-    // println!("{:?}", puzzle);
-
-    let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
-
-    for y in 0..puzzle.len() {
-        for x in 0..puzzle[0].len() {
-            match puzzle[y][x] {
-                '.' => continue,
-                character => add_or_insert(&mut antennas, character, x, y),
-            }
-        }
+    for (_, points) in antennas {
+        calculate_antinodes_for_antennas(&points, dimensions, &calculate_antinodes_one)
+            .into_iter()
+            .for_each(|(x, y)| puzzle[y][x] = '#')
     }
 
-    // println!("{:?}", antennas);
+    let count = puzzle.iter().flatten().filter(|&&c| c == '#').count();
+    println!("{count}");
 
-    for (_, points) in &antennas {
-        // println!("Character: {}, Points: {:?}", character, points);
+    Ok(())
+}
 
-        let points = calculate_antinodes_for_antennas(points, dimensions);
+fn calculate_antinodes_two(
+    lhs: (usize, usize),
+    rhs: (usize, usize),
+    dimensions: (usize, usize),
+) -> Vec<(usize, usize)> {
+    let (x1, y1) = lhs;
+    let (x2, y2) = rhs;
 
-        // println!("POINTS: {:?}", points);
+    let dx = x2 as isize - x1 as isize;
+    let dy = y2 as isize - y1 as isize;
 
-        points.into_iter().for_each(|(x, y)| {
-            if puzzle[y][x] != '#' {
-                puzzle[y][x] = '#'
-            }
-        })
+    let mut points = Vec::new();
+
+    let mut x = x1 as isize;
+    let mut y = y1 as isize;
+
+    while x >= 0 && x < dimensions.0 as isize && y >= 0 && y < dimensions.1 as isize {
+        points.push((x as usize, y as usize));
+        x -= dx;
+        y -= dy;
     }
 
-    // println!("{:?}", puzzle);
+    x = x2 as isize;
+    y = y2 as isize;
+
+    while x >= 0 && x < dimensions.0 as isize && y >= 0 && y < dimensions.1 as isize {
+        points.push((x as usize, y as usize));
+        x += dx;
+        y += dy;
+    }
+
+    points
+}
+
+#[test]
+fn part_two() -> Result<(), Error> {
+    let (mut puzzle, dimensions, antennas) = load_antennas()?;
+
+    for (_, points) in antennas {
+        calculate_antinodes_for_antennas(&points, dimensions, &calculate_antinodes_two)
+            .into_iter()
+            .for_each(|(x, y)| puzzle[y][x] = '#')
+    }
 
     let count = puzzle.iter().flatten().filter(|&&c| c == '#').count();
     println!("{count}");
