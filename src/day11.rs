7e7f622c7f9a327b0914163d29a581781009a913
Andreas Pfohl <mail@apfohl.com>
2024-12-29 17:28:28 +0100

    Added day 11 part one

--- src/day11.rs
+++ src/day11.rs
@@ -2,7 +2,7 @@ use crate::common::lines;
 use std::io::Error;
 
 fn load_puzzle() -> Result<Vec<usize>, Error> {
-    let puzzle = lines("inputs/day_11_test.txt")?
+    let puzzle = lines("inputs/day_11.txt")?
         .find_map(|line| match line {
             Ok(line) => Some(
                 line.split_whitespace()
@@ -16,11 +16,47 @@ fn load_puzzle() -> Result<Vec<usize>, Error> {
     Ok(puzzle)
 }
 
+fn handle_stone(stone: usize) -> Vec<usize> {
+    match stone {
+        0 => vec![1],
+        number if number.to_string().len() % 2 == 0 => {
+            let string = number.to_string();
+            let (left, right) = string.split_at(string.len() / 2);
+
+            vec![
+                left.parse::<usize>().unwrap(),
+                right.parse::<usize>().unwrap(),
+            ]
+        }
+        number => vec![number * 2024],
+    }
+}
+
+#[test]
+fn handle_stone_test() {
+    vec![
+        (0usize, vec![1usize]),
+        (23usize, vec![2usize, 3usize]),
+        (2usize, vec![4048usize]),
+    ]
+    .iter()
+    .for_each(|(stone, result)| assert_eq!(*result, handle_stone(*stone)))
+}
+
 #[test]
 fn part_one() -> Result<(), Error> {
-    let stones = load_puzzle()?;
+    let mut stones = load_puzzle()?;
 
     println!("{:?}", stones);
+
+    for _ in 0..25 {
+        stones = stones
+            .into_iter()
+            .flat_map(|stone| handle_stone(stone))
+            .collect()
+    }
+
+    println!("{}", stones.len());
 
     Ok(())
 }
@@ -30,6 +66,8 @@ fn part_two() -> Result<(), Error> {
     let stones = load_puzzle()?;
 
     println!("{:?}", stones);
+
+    println!("{}", stones.len());
 
     Ok(())
 }
