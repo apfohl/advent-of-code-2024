9869baa61fbfe25ec671918838c9fd26df6f33ea
Andreas Pfohl <mail@apfohl.com>
2025-01-02 14:15:21 +0100

    Added day 12 part two (test is working)

--- src/day12.rs
+++ src/day12.rs
@@ -2,7 +2,7 @@ use crate::common::lines;
 use std::io::Error;
 
 fn load_puzzle() -> Result<Vec<Vec<char>>, Error> {
-    let puzzle: Vec<Vec<char>> = lines("inputs/day_12.txt")?
+    let puzzle: Vec<Vec<char>> = lines("inputs/day_12_test.txt")?
         .filter_map(|line| match line {
             Ok(line) => Some(line.chars().collect()),
             Err(_) => None,
@@ -13,17 +13,62 @@ fn load_puzzle() -> Result<Vec<Vec<char>>, Error> {
 }
 
 const DIRECTIONS: [(isize, isize); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+const CORNERS: [[(isize, isize); 3]; 4] = [
+    [(0, -1), (1, -1), (1, 0)],
+    [(1, 0), (1, 1), (0, 1)],
+    [(0, 1), (-1, 1), (-1, 0)],
+    [(-1, 0), (-1, -1), (0, -1)],
+];
+
+fn count_corners(crops: &Vec<Vec<char>>, position: (usize, usize), plant: char) -> usize {
+    let mut corners = vec![];
+
+    for corner in CORNERS {
+        let mut new_corner = vec![];
+        for (x, y) in corner {
+            new_corner.push((
+                (position.0 as isize + x) as usize,
+                (position.1 as isize + y) as usize,
+            ))
+        }
+
+        corners.push(new_corner);
+    }
+
+    let mut result = 0usize;
+
+    corners.iter().for_each(|corner| {
+        let points = corner
+            .into_iter()
+            .map(
+                |&(x, y)| match crops.get(y).and_then(|row| row.get(x)) {
+                    Some(&p) if p == plant => true,
+                    _ => false,
+                },
+            )
+            .collect::<Vec<bool>>();
+
+        match points[..] {
+            [false, false, false] => result += 1,
+            [true, false, true] => result += 1,
+            _ => {}
+        }
+    });
+
+    result
+}
 
 fn dfs(
     crops: &Vec<Vec<char>>,
     visited: &mut Vec<Vec<bool>>,
     start: (usize, usize),
     crop: char,
-) -> (usize, usize) {
+) -> (usize, usize, usize) {
     let mut stack = vec![start];
 
     let mut fields = 0usize;
     let mut edges = 0usize;
+    let mut corners = 0usize;
 
     while let Some((x, y)) = stack.pop() {
         if visited[y][x] {
@@ -32,6 +77,8 @@ fn dfs(
         visited[y][x] = true;
 
         fields += 1;
+
+        corners += count_corners(crops, (x, y), crops[y][x]);
 
         for (dx, dy) in DIRECTIONS {
             let new_x = (x as isize + dx) as usize;
@@ -50,13 +97,14 @@ fn dfs(
         }
     }
 
-    (fields, edges)
+    (fields, edges, corners)
 }
 
 fn find_regions(crops: &Vec<Vec<char>>) {
     let mut visited = vec![vec![false; crops[0].len()]; crops.len()];
 
-    let mut price = 0usize;
+    let mut price_one = 0usize;
+    let mut price_two = 0usize;
 
     for (y, &ref row) in crops.iter().enumerate() {
         for (x, &crop) in row.iter().enumerate() {
@@ -64,28 +112,25 @@ fn find_regions(crops: &Vec<Vec<char>>) {
                 continue;
             }
 
-            let (fields, edges) = dfs(crops, &mut visited, (x, y), crop);
-            price += fields * edges;
+            let (fields, edges, corners) = dfs(crops, &mut visited, (x, y), crop);
+            price_one += fields * edges;
+            price_two += fields * corners;
 
-            // println!("{crop}: {fields} {edges}")
+            println!("{crop}: {fields} {edges} {corners}")
         }
     }
 
-    println!("{price}")
+    println!("Part one: {price_one}");
+    println!("Part two: {price_two}")
 }
 
 #[test]
-fn part_one() -> Result<(), Error> {
+fn solve() -> Result<(), Error> {
     let crops = load_puzzle()?;
 
     // println!("{:?}", crops);
 
     find_regions(&crops);
 
-    Ok(())
-}
-
-#[test]
-fn part_two() -> Result<(), Error> {
     Ok(())
 }
